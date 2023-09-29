ccfacd11a458e229e6909870c5e307746186a5a6
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2023-09-29 19:24:55 +0000

    Hash

---
+++ Hash.h
@@ -0,0 +1,20 @@
+#ifndef EstruturaDeDados_Hash_h
+#define EstruturaDeDados_Hash_h
+#include "DoublyLinkedList.h"
+#define MAX 10
+
+typedef struct HashStruct {
+    DoublyLinkedList hashes[MAX];
+    int size;
+}HashStruct;
+
+void initHash(HashStruct *hashStruct);
+bool isHashEmpty(HashStruct *hashStruct);
+int hash(char *key);
+int put(HashStruct *hashStruct, char *key, void *data, compare equal);
+bool containsKey(HashStruct *hashStruct, char *key, compare equal);
+void* get(HashStruct *hashStruct, char *key, compare equal);
+void* removeKey(HashStruct *hashStruct, char *key, compare equal);
+void showHashStruct(HashStruct *hashStruct);
+
+#endif
