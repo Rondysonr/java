e5c7361a14c886f357d2a039f4037d5fd68c6164
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2024-02-05 21:10:05 +0000

    cap3

---
+++ cap3/exercicio1.java
@@ -0,0 +1,7 @@
+public class exercicio1{
+	public static void main(String args[]){
+		for(int i=150; i<301; i++){
+			System.out.println(i);
+		}	
+	}
+}
---
+++ cap3/exercicio3.java
@@ -0,0 +1,9 @@
+public class exercicio2{
+	public static void main(String args[]){
+		int j=0;
+        for(int i=0; i<1001; i++){
+			j=j+i;
+		}
+        System.out.println(j);	
+	}
+}
