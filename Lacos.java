ccf094bcb94e2a90e21dfb7d0031a74e085f5786
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2024-02-05 20:44:41 +0000

    soma e laço

---
+++ Lacos.java
@@ -0,0 +1,10 @@
+public class Lacos{
+	public static void main(String args[]){
+        int x=15;
+        if (x%2==0){
+            System.out.println("par");
+        }
+        else
+		System.out.println("impar");
+	}
+}
---
+++ Soma.java
@@ -0,0 +1,7 @@
+public class Soma{
+	public static void main(String args[]){
+        int x=10;
+        long y= 30;
+		System.out.println(x+y);
+	}
+}
