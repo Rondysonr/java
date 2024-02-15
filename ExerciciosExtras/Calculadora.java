754d1487304f20a970edb061b4ba159d917720ae
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2024-02-15 21:09:57 +0000

    calculadora

---
+++ ExerciciosExtras/Calculadora.java
@@ -0,0 +1,13 @@
+public class Cal { 
+      
+       public double soma(double x, double y){
+            return x+y;
+       }
+       /*public double menos(double x, double y){
+            return x-y;
+       }public double mult(double x, double y){
+            return x*y;
+       }public double dividi(double x, double y){
+            return x/y;
+       }*/
+}
---
+++ ExerciciosExtras/TestaCalculadora.java
@@ -0,0 +1,7 @@
+public class TestaCalculadora{
+	public static void main(String args[]){
+       Cal c1 = new Cal();
+       
+       System.out.println("soma ="+ c1.soma(10,10));
+	}
+}
