f43f7ff409c3685b08c84e5c3dee17462be440e7
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2024-02-22 21:19:15 +0000

    teclado

---
+++ ExerciciosExtras/Soma.java
@@ -0,0 +1,17 @@
+
+public class Soma {
+
+	public static void main(String[] args) {
+		String texto = Teclado.lerString("Java é legal");
+		int[]  x= new int[5];
+		for (int i = 1; i<=5; i++) {	
+		x[i] = Teclado.lerInteiro("digite um numero"+ i + ":");
+		//System.out.println(texto);
+		}
+		for (int i = 1; i<=5; i++) {
+			
+		}
+		
+	}
+
+}
