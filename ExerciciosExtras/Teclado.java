8cf3b7c5784abd77c809e5ac4afd2203d81b2199
Rondysonr <126256862+Rondysonr@users.noreply.github.com>
2024-07-25 15:22:52 -0400

    Update Teclado.java

--- ExerciciosExtras/Teclado.java
+++ ExerciciosExtras/Teclado.java
@@ -6,10 +6,20 @@ public class Teclado {
 		Scanner scann = new Scanner (System.in);
 		return scann.nextInt();
 	}
+	public static long lerLong(String rotulo) {
+		System.out.println(rotulo);
+		Scanner scann = new Scanner (System.in);
+		return scann.nextLong();
+	}
 	public static double lerDouble(String rotulo) {
 		System.out.println(rotulo);
 		Scanner scann = new Scanner (System.in);
 		return scann.nextDouble();
+	}
+	public static float lerFloat(String rotulo) {
+		System.out.println(rotulo);
+		Scanner scann = new Scanner (System.in);
+		return scann.nextFloat();
 	}
 	public static String lerString(String rotulo) {
 		System.out.println(rotulo);
